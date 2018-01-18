2d64bb0422cdce2bec7502fedd118b04ea5355f6
Vanessa Muller <vane9090@gmail.com>
2018-01-18 13:59:40 -0500

    configured local database

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as RxDB from 'rxdb';
+import { QueryChangeDetector} from 'rxdb';
+import {schema} from './Schema';
+
+QueryChangeDetector.enable();
+QueryChangeDetector.enableDebugging();
+
+RxDB.plugin(require('pouchdb-adapter-idb'));
+RxDB.plugin(require('pouchdb-adapter-http'));
+
+const syncURL = 'http://localhost:5984/';
+const dbName = 'chatdb';
 
 class App extends Component {
+  async createDatabase () {
+    const db = await RxDB.create(
+      {name: dbName, adapter: 'idb', password: '12345678'}
+    );
+
+    db.waitForLeadership().then(() => {
+      document.title = 'CROWN' + document.title;
+    });
+
+    const messagesCollection = await db.collection ({
+      name: 'messages',
+      schema: schema
+    });
+
+    messagesCollection.sync({ remote: syncURL + dbName + '/' });
+
+    return db;
+  }
   render() {
     return (
       <div className="App">
